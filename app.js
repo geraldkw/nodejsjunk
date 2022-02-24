0a80927704afeae1525d00360e4aee17210b00f2
w600119 <jwisniewski@woodmen.org>
2022-02-24 16:00:06 -0600

    today

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const _ = require('lodash');
-
-const items = [1,[2,[3,[4]]]]
-
-const newItems = _.flattenDeep(items);
-
-console.log(newItems);
+const {readFile,writeFile} = require('fs');
+console.log("start");
+readFile('./content/first.txt', 'utf8', (err, result) => {
+	if (err) {
+		console.log(err);
+		return null;
+	} 
+	const first = result;
+	readFile('./content/second.txt', 'utf8', (err, result) => {
+		if (err) {
+			console.log(err);
+			return null;
+		}
+		const second = result;
+		writeFile('./content/results.txt', `here is the result ${first} ${second}`,
+		(err, result) => {
+			if (err) {
+				return;
+			}
+			console.log("done with this task");
+		});
+	})
+})
+console.log("starting next task");
