1e218f59e3432756922e027dfa32f7f118a815d0
w600119 <jwisniewski@woodmen.org>
2022-04-08 10:43:07 -0500

    more changes

--- node-express-course/04-store-api/starter/controllers/products.js
+++ node-express-course/04-store-api/starter/controllers/products.js
@@ -1,13 +1,12 @@
 const Product = require('../models/product.js');
 
 const getAllProductsStatic = async (req, res) => {
-	const search = 'a'
-	const products = await Product.find({ name: { $regex:search, $options: 'i' } });
+	const products = await Product.find({price: {$gt:30} }).select('name price').sort('price');
 	res.status(200).json({products, nbHits: products.length});
 }
 
 const getAllProducts = async (req, res) => {
-	const {featured,company,name} = req.query
+	const {featured,company,name,sort,fields, numericFilters} = req.query
 	const queryObject = {}
 
 	if (featured) {
@@ -17,10 +16,51 @@ const getAllProducts = async (req, res) => {
 		queryObject.company = company;
 	}
 	if (name) {
-		queryObject.name = name;
+		queryObject.name = { $regex: name, $options: 'i' };
+	}
+	if (numericFilters) {
+		const operatorMap = {
+			'>':'$gt',
+			'<':'$lt',
+			'>=':'$gte',
+			'<=':'$lte',
+			'=':'$eq',
+		}
+
+		const regex = /\b(<|>|>=|<=|=)\b/g
+		let filters = numericFilters.replace(regex, (match)=>`-${operatorMap[match]}-`)
+		
+		const options = ['price', 'rating'];
+		filters = filters.split(',').forEach((item)=>{
+			const [field, operator, value] = item.split('-');
+			if (options.includes(field)) {
+				queryObject[field] = {[operator]: Number(value) }
+			}
+		})
 	}
 
-	const products = await Product.find(queryObject);
+	let result = Product.find(queryObject);
+	if (sort) {
+		const sortList = sort.split(',').join(' ');
+		result.sort(sortList);
+	} else {
+		result = result.sort('createdAt');
+	}
+	if (fields) {
+		const fieldsList = fields.split(',').join(' ');
+		result.select(fieldsList);
+	} else {
+		result = result.select({});
+	}
+	const page = Number(req.query.page) || 1;
+	const limit = Number(req.query.limit) || 10;
+	const skip = (page-1)*limit;
+	result = result.skip(skip).limit(limit);
+
+	//23 products, limit should show 4 pages if limit is 7 
+	//skip is number of pages * items per page which is number to skip to page forward
+
+	const products = await result;
 	res.status(200).json({products, nbHits: products.length});
 }
 
---
+++ node-express-course/05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,30 @@
+//check for username, password in the requrest
+//if exist create new JWT
+//send back to front end
+//
+//set up authentication so only requests with JWT can access the dashboard
+//
+const jwt = require('jsonwebtoken')
+
+const CustomAPIError = require('../errors/custom-error.js')
+
+const login = async (req, res) => { 
+	const {username, password} = req.body
+	//other options for below validation, Mongoose validation, Joi(another validation pkg)
+	if (!username || !password) {
+		throw new CustomAPIError('Please provide email and password', 400);
+	}
+	const id = new Date().getDate() //for example only
+	const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'})
+	res.status(200).json({msg: 'user created',token});
+}
+
+const dashboard = async (req, res) => {
+	console.log(req.user);
+	const luckyNumber = Math.floor(Math.random()*100)
+	res.status(200).json({msg: `Hello, ${req.user.username}`, secret: `Here is number ${luckyNumber}`})
+}
+
+module.exports = {
+	login, dashboard
+}
